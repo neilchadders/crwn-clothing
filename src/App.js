af6f96cd571cc3211b3b6a1ad285ed2b5da7eddd
Neil Chadwick <neilchadders@hotmail.com>
2023-01-11 08:41:52 +0000

    adding initial sass and creating basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './categories.styles.scss';
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Trainers'
+    },
+    {
+      id: 4,
+      title: 'Mens'
+    },
+    {
+      id: 5,
+      title: 'Womens'
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
+
   );
-}
+};
 
 export default App;
