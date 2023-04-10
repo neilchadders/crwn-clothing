27da3b2aab3ff8781c28a385e0cc3225cb9a047c
Neil Chadwick <neilchadders@hotmail.com>
2023-04-10 18:49:15 +0100

    fix problem with react - router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 //import Authentication from './routes/authentication/authentication.component';
 
 
-const Navigation = () => {
-  <div>
-    <div>
-      return <h1>I am The Navigation Bar</h1>
-    </div>
-    <Outlet />
-  </div>
-
-}
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -22,7 +13,7 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
 
